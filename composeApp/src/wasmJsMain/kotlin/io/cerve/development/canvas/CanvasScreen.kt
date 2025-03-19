7107852d74816680a4cdbb08dea985c4a821e287
cerver1 <cerve.v55@gmail.com>
2025-03-19 11:57:25 -0400

    canvas lib updated and draw types improved.

--- composeApp/src/wasmJsMain/kotlin/io/cerve/development/canvas/CanvasScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/io/cerve/development/canvas/CanvasScreen.kt
@@ -1,5 +1,7 @@
 package io.cerve.development.canvas
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,23 +10,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Redo
 import androidx.compose.material.icons.automirrored.filled.Undo
+import androidx.compose.material.icons.filled.Brush
 import androidx.compose.material.icons.filled.CleaningServices
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Draw
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.outlined.Brush
+import androidx.compose.material.icons.outlined.DesignServices
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.animation.AnimatedVisibility
 import com.cerve.development.ui.canvas.component.CerveDrawCanvas
 import com.cerve.development.ui.canvas.model.CerveCanvasInteractionType
 import com.cerve.development.ui.canvas.operators.rememberCerveDrawCanvasState
 
 @Composable
-fun App() {
+fun CanvasScreen() {
     MaterialTheme {
         val canvas = rememberCerveDrawCanvasState()
 
@@ -38,30 +43,31 @@ fun App() {
                         verticalAlignment = Alignment.CenterVertically
                     ) {
 
-                        BadgedBox(
-                            badge = {
-                                androidx.compose.animation.AnimatedVisibility(canvas.interactionType == null) {
-                                    Badge {
-                                        Text(
-                                            modifier = Modifier.padding(5.dp),
-                                            text = "select"
-                                        )
-                                    }
-                                }
-                            }
+                        IconButton(
+                            colors = if (canvas.interactionType == CerveCanvasInteractionType.Brush) {
+                                IconButtonDefaults.filledIconButtonColors()
+                            } else IconButtonDefaults.iconButtonColors(),
+                            onClick = { canvas.onChangeInteractionType(CerveCanvasInteractionType.Brush) }
                         ) {
-                            IconButton(
-                                colors = if (canvas.interactionType == CerveCanvasInteractionType.Brush) {
-                                    IconButtonDefaults.filledIconButtonColors()
-                                } else IconButtonDefaults.iconButtonColors(),
-                                onClick = { canvas.onChangeInteractionType(CerveCanvasInteractionType.Brush) }
-                            ) {
-                                Icon(
-                                    modifier = Modifier,
-                                    imageVector = Icons.Default.Draw,
-                                    contentDescription = null
-                                )
-                            }
+                            Icon(
+                                modifier = Modifier,
+                                imageVector = Icons.Default.Draw,
+                                contentDescription = null
+                            )
+                        }
+
+
+                        IconButton(
+                            colors = if (canvas.interactionType == CerveCanvasInteractionType.AssistedBrush) {
+                                IconButtonDefaults.filledIconButtonColors()
+                            } else IconButtonDefaults.iconButtonColors(),
+                            onClick = { canvas.onChangeInteractionType(CerveCanvasInteractionType.AssistedBrush) }
+                        ) {
+                            Icon(
+                                modifier = Modifier,
+                                imageVector = Icons.Outlined.DesignServices,
+                                contentDescription = null
+                            )
                         }
 
                         IconButton(
@@ -72,7 +78,7 @@ fun App() {
                         ) {
                             Icon(
                                 modifier = Modifier,
-                                imageVector = Icons.Outlined.Brush,
+                                imageVector = Icons.Outlined.Edit,
                                 contentDescription = null
                             )
                         }
@@ -122,7 +128,13 @@ fun App() {
                 interactionType = canvas.interactionType,
                 eraserLines = canvas.eraserLines,
                 currentLines = canvas.currentLines,
-                currentLineCandidates = canvas.currentLineCandidates
+                currentLineCandidates = canvas.currentLineCandidates,
+                transformationScale = canvas.transformationScale,
+                transformationRotation = canvas.transformationRotation,
+                translationOffset = canvas.transformationOffset,
+                onChangeTransformationScale = canvas::onChangeTransformationScale,
+                onChangeTransformationRotation = canvas::onChangeTransformationRotation,
+                onChangeTransformationOffset = canvas::onChangeTransformationOffset
             )
         }
 
--- composeApp/src/wasmJsMain/kotlin/io/cerve/development/canvas/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.cerve.development.canvas
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
